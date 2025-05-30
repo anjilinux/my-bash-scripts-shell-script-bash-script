77909e93683cae6d8713eb32e7919af7773090f1
ANJIREDDY -Architect - AI-ML   [ BNYMELLON.COM -PUNE] <94956350+anjilinux@users.noreply.github.com>
2025-05-30 07:20:23 +0530

    Update k8s-shell-script.sh

--- k8s-shell-script.sh
+++ k8s-shell-script.sh
@@ -1,46 +1,99 @@
+## 24.04 UBUNTU
+https://www.cherryservers.com/blog/install-kubernetes-ubuntu
 
-####### NANO add  data 
-nano /etc/modules-load.d/containerd.conf
+sudo hostnamectl hostname worker2
 
+set -x
+
+sudo swapoff -a
+swapon --show
+
+sudo modprobe overlay
+
+sudo modprobe br_netfilter
+
+sudo tee /etc/modules-load.d/k8s.conf <<EOF
 overlay
 br_netfilter
+EOF
 
-#################################
 
- nano /etc/sysctl.d/kubernetes.conf
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+
+sudo apt update
+
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 
-################  sh-shell-script ###################################
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker run hello-world
 
 
-swapoff -a
-sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+#sudo systemctl status docker
 
-modprobe overlay
-modprobe br_netfilter
+sudo systemctl enable docker
 
-apt-get update
+sudo mkdir /etc/containerd
+
+sudo sh -c "containerd config default > /etc/containerd/config.toml"
+
+sudo sed -i 's/ SystemdCgroup = false/ SystemdCgroup = true/' /etc/containerd/config.toml
+
+sudo systemctl restart containerd.service
+
+#sudo systemctl status containerd.service
+
+
+sudo apt-get update
+
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-apt update
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
-
-mkdir -p /etc/containerd
-containerd config default | tee /etc/containerd/config.toml > /dev/null
-systemctl restart containerd
+sudo systemctl enable --now kubelet
 
 
+#############  MASTER ONLY MASTER SETUP ###########
+
+sudo kubeadm init --pod-network-cidr=10.11.0.0/16 
+
+sudo mkdir -p $HOME/.kube
+
+sudo sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+
+sudo sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+ kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
+
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml -O
 
 
+sed -i 's/cidr: 192\.168\.0\.0\/16/cidr: 10.11.0.0\/16/g' custom-resources.yaml
 
 
+kubectl create -f custom-resources.yaml
+
+kubectl get nodes
 
